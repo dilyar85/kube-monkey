7f3fda2caa62a93672142acf2dd62da26c09db7f
Saifuding Diliyaer <sdiliyaer@box.com>
2019-03-16 13:53:49 -0700

    Add go import path

--- .travis/check_workspace.sh
+++ .travis/check_workspace.sh
@@ -5,7 +5,7 @@ if [ "$(git status --porcelain | wc -l)" -ne "0" ]; then
   echo "Dirty workspace detected. This typically indicates 'go fmt' changed some files?" \
        "\n Run 'make' locally to verify."
   git status --porcelain
-  echo "$(git status --porcelain | wc -l"
+  echo "$(git status --porcelain | wc -l)"
   exit 1
 else
   exit 0
